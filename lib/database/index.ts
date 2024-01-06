b06b719befea69dda756f8becafd70c398408161
Sattwik <hellosattwik@gmail.com>
2024-01-06 18:03:07 +0530

    fix: Fix error handling in connectToDatabase function

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,3 +1,4 @@
+import { MongoError } from 'mongodb';
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -7,7 +8,7 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new DatabaseError('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: 'evently',
@@ -17,4 +18,28 @@ export const connectToDatabase = async () => {
   cached.conn = await cached.promise;
 
   return cached.conn;
+}
+
+export class DatabaseError extends Error {
+  readonly code: number | undefined | string;
+  readonly cause: any;
+
+  constructor(data: unknown) {
+    super();
+
+    if (typeof data === "string") {
+      this.message = data;
+    } else if (data instanceof MongoError) {
+      this.message = data.message;
+      this.cause = data;
+      this.code = data.code;
+    } else if (data instanceof Error) {
+      this.message = data.message;
+      this.cause = data;
+    } else {
+      this.message = "Unexpected error happened";
+    }
+
+    this.name = "DatabaseError";
+  }
 }
